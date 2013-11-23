07c05932833019915260d60d3f0bc8a7ae0c42bb
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2013-11-23 11:15:01 -0600

    test commit

--- JavaTest.java
+++ JavaTest.java
@@ -1,7 +1,14 @@
 import java.util.*;
+import java.io.*;
+import java.net.*;
 
 public class JavaTest {
 	public static void main(String[] args) {
+		JavaTest instance = new JavaTest();
+		instance.socketTest();
+	}
+
+	public void lhsTest() {
 		Set<Integer> s = new LinkedHashSet<Integer>();
 		s.add(1);
 		s.add(2);
@@ -12,5 +19,27 @@ public class JavaTest {
 		System.out.println(s);
 		System.out.println(s.iterator().next());
 		System.out.println(s.iterator().next());
+	}
+
+	void socketTest() {
+		String serverHostname = new String ("127.0.0.1");
+
+		Socket socket = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+
+		try {
+			socket = new Socket(serverHostname, 10007);
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(
+						socket.getInputStream()));
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host: " + serverHostname);
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for "
+					+ "the connection to: " + serverHostname);
+			System.exit(1);
+		}
 	}
 }
