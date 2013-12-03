3d6b1634ee2fa414d47a81a7721da6cf1395a797
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2013-12-03 01:43:35 -0600

    Updated initialization and update_network

--- pgm/message_passing.py
+++ pgm/message_passing.py
@@ -8,6 +8,7 @@ import operator
 import copy
 import pprint as pp
 
+E = {}
 # Returns values of a variable X
 # In the example graph, all variables are binary
 def values(X):
@@ -32,6 +33,7 @@ def find_roots(dag):
 	return roots
 
 def initialize_network(dag):
+	global E
 	inv_dag = inverse_graph(dag)
 	lambdas = {}
 	pis = {}
@@ -50,10 +52,47 @@ def initialize_network(dag):
 			pis[var][c] = {}
 			for x in values(c):
 				pis[var][c][x] = 1
-
 	
+	for root in find_roots(dag):
+		for x in values(root):
+			pis[root][x] = prob[root]
+			# P(root|e) = P(root)
+		for c in dag[root]:
+			send_pi_msg(root, c)
 
-def example_graph():
+# X is a specific assignment: dict of Variable : value
+# E is the dict of evidence variables and their values
+def update_network(dag, V):
+	# update evidence with the current assignment
+	E = E.update(V)
+
+	for var in V:
+		for v in values(var):
+			if v == V[var]:
+				lambdas[var][v] = 1
+				pis[var][v] = 1
+				prob[var][v] = 1
+			else:
+				lambdas[var][v] = 0
+				pis[var][v] = 0
+				prob[var][v] = 0
+	
+	lambda_recipients = set()
+	for var in V:
+		for p in inv_dag[var]:
+			if p not in E:
+				send_lambda_msg(dag, var, p)
+
+	for var in V:
+		for c in dag[var]:
+			send_pi_msg(dag, var, c)
+
+def send_lambda_msg(dag, child, parent):
+	for x in values(parent):
+		
+
+
+def example_input():
 	G = {}
 	G['A'] = set(['C'])
 	G['B'] = set(['C'])
