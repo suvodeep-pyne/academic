09312165fe9aa6879fb79fe42a80ae76789643fe
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2014-01-04 11:49:54 +0530

    updated.

--- spoj/sud.cpp
+++ spoj/sud.cpp
@@ -10,21 +10,25 @@ int poss[MAX];
 
 inline bool is_fixed(int n)
 {
-	return !(n & (n - 1));
+	return !(poss[n] & (poss[n] - 1));
 }
 
 // val: Find the set bit
 inline int set_bit(int n)
 {
 	int val = 0;
-	while (!(n & ( 1 << val))) ++val;
+	while (!(poss[n] & ( 1 << val))) ++val;
 	return val;
 }
 
-void reduce(int start)
+void reduce(vector<int> given)
 {
 	queue<int> q;
-	q.push(start);
+	for (vector<int>::iterator ii = given.begin();
+		ii != given.end(); ++ii)
+	{
+		q.push(*ii);
+	}
 
 	while (q.size() > 0)
 	{
@@ -32,21 +36,21 @@ void reduce(int start)
 		q.pop();
 
 		// Only 1 bit is set. It is a power of 2
-		if (is_fixed(poss[p]))
+		if (is_fixed(p))
 		{
-			int val = set_bit(poss[p]);
+			int val = set_bit(p);
 
 			for (int i = 0; i < 9; ++i)
 			{
 				int h = (p / 9) * 9 + i;
-				if (h != p)
+				if (h != p && !is_fixed(h))
 				{
 					poss[h] &= ~(1 << val);
 					if (is_fixed(h))
 						q.push(h);
 				}
 				int v = p % 9 + i * 9;
-				if (v != p)
+				if (v != p && !is_fixed(v))
 				{
 					poss[v] &= ~(1 << val);
 					if (is_fixed(v))
@@ -59,8 +63,8 @@ void reduce(int start)
 			{
 				for (int j = sj; j < sj + 3; ++j)
 				{
-					int ii = si * 9 + sj;
-					if (ii != p)
+					int ii = i * 9 + j;
+					if (ii != p && !is_fixed(ii))
 					{
 						poss[ii] &= ~(1 << val);
 						if (is_fixed(ii))
@@ -70,6 +74,30 @@ void reduce(int start)
 			}
 		}
 	}
+}
+
+void showPoss()
+{
+	long double posscount = 1;
+	for (int i = 0; i < 81; i++)
+	{
+		printf("%3x ", poss[i]);
+		int c = 0;
+		for(int j = 0; j < 9; j++)
+		{
+			if(poss[i] & (1 << j))
+			{
+				printf("%d", 1 + j);
+				++c;
+			}
+			else printf(".");
+		}
+		printf(" ");
+		if ((i + 1) % 9 == 0) cout << endl;
+		posscount *= c;
+	}
+	cout << endl;
+	cout << "Possibility Count:" << posscount << endl;
 }
 
 void printPoss()
@@ -86,8 +114,8 @@ void print()
 {
 	for (int i = 0; i < 81; i++)
 	{
-		if (is_fixed(poss[i]))
-			printf("%d", 1 + set_bit(poss[i]));
+		if (is_fixed(i))
+			printf("%d", 1 + set_bit(i));
 		else
 			printf(".");
 		if ((i + 1) % 9 == 0) cout << endl;
@@ -100,19 +128,29 @@ void solve()
 	char prob[MAX];
 	cin >> prob;
 
+	vector<int> given;
 	for (int i = 0; i < MAX; i++)
 	{
 		if (prob[i] == '.')
 			poss[i] = 0x1FF;
 		else if (prob[i] >= '1' && prob[i] <= '9')
+		{
 			poss[i] = 1 << (prob[i] - '1');
+			given.push_back(i);
+		}
 		else
 			poss[i] = 0;
 	}
 
 	print();
-	// reduce();
-	printPoss();
+	// printPoss();
+	showPoss();
+
+	reduce(given);
+	
+	print();
+	// printPoss();
+	showPoss();
 }
 
 int main() 
